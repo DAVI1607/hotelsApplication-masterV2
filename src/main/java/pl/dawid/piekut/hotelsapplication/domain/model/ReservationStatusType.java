69210e0e820ce4adcd8edc1e0ac65816f15fdba7
DAVI1607 <73023659+DAVI1607@users.noreply.github.com>
2021-12-16 21:22:08 +0100

    Initial commit

---
+++ src/main/java/pl/dawid/piekut/hotelsapplication/domain/model/ReservationStatusType.java
@@ -0,0 +1,5 @@
+package pl.dawid.piekut.hotelsapplication.domain.model;
+
+public enum ReservationStatusType {
+    NEW,IN_PROGRESS,COMPLETE
+}
---
+++ src/main/java/pl/dawid/piekut/hotelsapplication/remote/rest/dto/request/AddReservationDto.java
@@ -0,0 +1,31 @@
+package pl.dawid.piekut.hotelsapplication.remote.rest.dto.request;
+
+import pl.dawid.piekut.hotelsapplication.remote.rest.dto.common.PersonReservationDto;
+import pl.dawid.piekut.hotelsapplication.remote.rest.dto.common.RoomReservationDto;
+
+import java.util.List;
+
+public class AddReservationDto {
+    private List<RoomReservationDto> hotels;
+    private PersonReservationDto person;
+
+    public AddReservationDto() {
+    }
+
+    public PersonReservationDto getPerson() {
+        return person;
+    }
+
+    public void setPerson(PersonReservationDto person) {
+        this.person = person;
+    }
+
+    public List<RoomReservationDto> getHotels() {
+        return hotels;
+    }
+
+    public void setHotels(List<RoomReservationDto> hotels) {
+        this.hotels = hotels;
+    }
+}
+
